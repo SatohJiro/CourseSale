80ffe1a1e7e38e2057aa063be478d2476718c72e
nguyenhoaibao1 <nbao1467@gmail.com>
2022-12-07 19:10:37 +0700

    admin profile

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,14 +4,14 @@ import Home from "~/pages/Home";
 import Courses from "~/pages/Courses";
 
 import AddCourses from "~/pages/AddCourse";
-import Login from "~/pages/Login/Login";
+// import Login from "~/pages/Login/Login";
 
 
 const publicRoutes = [
   { path: ROUTE_NAME.ROOT, component: Home },
   { path: ROUTE_NAME.COURSES, component: Courses },
   { path: ROUTE_NAME.ADDCOURSE, component: AddCourses },
-  { path: ROUTE_NAME.LOGIN, component: Login },
+  // { path: ROUTE_NAME.LOGIN, component: Login },
 
 ];
 const privateRoutes = [];
