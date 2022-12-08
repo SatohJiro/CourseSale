94ea5b4acd7055a0f83532aa09086579718ea0a7
Satoh Jiro <trananhq2345@gmail.com>
2022-12-08 22:02:27 +0700

    fix warning

--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -1,10 +1,10 @@
-import classNames from 'classnames/bind';
+// import classNames from 'classnames/bind';
 import Logo from '../../components/Logo';
 import Search from '../../components/Search';
 import UserNav from '../../components/UserNav';
-import style from './Header.module.scss'
+// import style from './Header.module.scss'
 
-const cx = classNames.bind(style);
+// const cx = classNames.bind(style);
 function Header() {
     return (
         <div className="bg-[#202129] flex justify-between h-[65px] border-b-2 border-gray-700">
